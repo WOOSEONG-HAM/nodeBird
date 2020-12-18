9cfbebbbc48f02410b1edfe1e5fa395f87af0f0c
WOOSEONG-HAM <wooseongham@gmail.com>
2020-12-19 01:39:38 +0900

    회원가입, 로그인, 기타 화면 추가

---
+++ react-nodebird/front/components/PostCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Card, Button, Avatar } from 'antd';
+import PropTypes from 'prop-types';
+import { RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined } from '@ant-design/icons';
+
+const PostCard = ({ post }) => {
+  return (
+    <Card
+      key={+post.createdAt}
+      cover={post.img && <img alt="example" src={post.img} />}
+      actions={[
+        <RetweetOutlined />,
+        <HeartOutlined />,
+        <MessageOutlined />,
+        <EllipsisOutlined />,
+      ]}
+      extra={<Button>팔로우</Button>}
+    >
+      <Card.Meta
+        avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+        title={post.User.nickname}
+        description={post.content}
+      />
+    </Card>
+  );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    User: PropTypes.object,
+    content: PropTypes.string,
+    img: PropTypes.string,
+    createdAt: PropTypes.object,
+  }),
+};
+
+export default PostCard;
--- react-nodebird/front/pages/index.js
+++ react-nodebird/front/pages/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import Head from 'next/head';
-import AppLayout from '../components/AppLayout';
+import PostForm from '../components/PostForm';
+import PostCard from '../components/PostCard';
+
+const dummy = {
+  isLoggedIn: true,
+  imagePaths: [],
+  mainPosts: [{
+    User: {
+      id: 1,
+      nickname: '함우성',
+    },
+    content: '첫 번째 게시글',
+    img: './wooseong.jpg',
+  }],
+};
 
 const Home = () => {
   return (
-    <>
-      <Head>
-        <title>NodeBird</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css" />
-      </Head>
-      <AppLayout>
-        <div>Hello, Next!</div>
-      </AppLayout>
-    </>
+    <div>
+      {dummy.isLoggedIn && <PostForm />}
+      {dummy.mainPosts.map((c) => {
+        return (
+          <PostCard key={c} post={c} />
+        );
+      })}
+    </div>
   );
 };
 
